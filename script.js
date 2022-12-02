f0d0e3171663633248a228f56fda78945d828c7e
darg3 <strix1.io@protonmail.com>
2022-12-02 19:14:37 +0200

    tip per person feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn = document.querySelector(".btn");
 const output = document.getElementById("percValue");
 
 const btn_rst = document.querySelector(".btn_reset");
+const peopleN = document.querySelector(".peopleNo");
 
 function clickCalcTip() {
   // console.log(document.querySelector(".checkValue").value);
@@ -12,11 +13,15 @@ function clickCalcTip() {
   // Select the fields(meal value and tips percentage)
   check = document.getElementById("checkValue");
   percent = document.getElementById("percValue");
+  // peopleNumber = document.getElementById("peopleNo")
   // tipValue = document.getElementById("tipVal");
 
-  // Calculate the tip
+  // Calculate the total tip
   tipValue = (percValue.value / 100) * check.value;
   // console.log(tipValue);
+
+  // Calculate tip per person
+  tipPerPerson = tipValue / peopleN.value;
 
   // Calculate total including tip
   totalToPay = tipValue + parseInt(check.value);
@@ -25,6 +30,17 @@ function clickCalcTip() {
   // Populate fields(tip and total incl. tip)
   document.getElementById("tipVal").value = tipValue;
   document.getElementById("totalPlusTip").value = totalToPay;
+  document.getElementById("tippp").value = tipPerPerson;
+
+  // tipVal.classList.remove("hideme");
+  // document.querySelector(".tip__val").classList.remove("hideme");
+  // setTimeout(function () {
+  //   totalPlusTip.classList.remove("hideme");
+  // }, 2000);
+
+  // setTimeout(function () {
+  //   tippp.classList.remove("hideme");
+  // }, 3000);
 }
 
 btn.addEventListener("click", clickCalcTip);
